790d36061466213c2b0543dba3eca988fddf16c8
Lgmrszd <m.v.bobrov@yandex.ru>
2022-01-28 01:41:07 +0300

    Current state of repo - Updated dependencies - Using Item Registrate from Create - Some localization files

--- src/main/java/com/lgmrszd/compressedcreativity/ModItems.java
+++ src/main/java/com/lgmrszd/compressedcreativity/ModItems.java
@@ -1,18 +1,25 @@
 package com.lgmrszd.compressedcreativity;
 
+import com.lgmrszd.compressedcreativity.items.potato_cannon_mk2.PotatoCannonMk2;
+import com.simibubi.create.content.curiosities.weapons.PotatoCannonItemRenderer;
+import com.simibubi.create.foundation.data.AssetLookup;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.repack.registrate.util.entry.ItemEntry;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS
-            = DeferredRegister.create(ForgeRegistries.ITEMS, CompressedCreativity.MOD_ID);
 
-//    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item",
-//            () -> new Item(new Item.Properties().tab(ModGroup.MAIN)));
+    private static final CreateRegistrate REGISTRATE = CompressedCreativity.registrate()
+            .itemGroup(() -> ModGroup.MAIN);
+
+//    public static final DeferredRegister<Item> ITEMS
+//            = DeferredRegister.create(ForgeRegistries.ITEMS, CompressedCreativity.MOD_ID);
+
 
     public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
+//        ITEMS.register(eventBus);
     }
 }
