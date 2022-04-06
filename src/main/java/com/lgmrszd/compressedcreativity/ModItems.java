8dc36f2b07fc35b32200643727029d883f334820
Lgmrszd <m.v.bobrov@yandex.ru>
2022-04-07 00:16:46 +0300

    Goggles Upgrade and Goggles Upgrade Item (currently not working)

--- src/main/java/com/lgmrszd/compressedcreativity/ModItems.java
+++ src/main/java/com/lgmrszd/compressedcreativity/ModItems.java
@@ -1,7 +1,17 @@
 package com.lgmrszd.compressedcreativity;
 
+import com.lgmrszd.compressedcreativity.index.CCUpgradeItem;
+import com.lgmrszd.compressedcreativity.index.CCUpgrades;
+import com.lgmrszd.compressedcreativity.items.EngineerGogglesUpgradeItem;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import me.desht.pneumaticcraft.api.item.PNCUpgrade;
+import me.desht.pneumaticcraft.common.core.ModUpgrades;
+import me.desht.pneumaticcraft.common.item.UpgradeItem;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.stream.IntStream;
+
 public class ModItems {
 
     private static final CreateRegistrate REGISTRATE = CompressedCreativity.registrate()
@@ -9,6 +19,22 @@ public class ModItems {
 
 
     public static void register(IEventBus eventBus) {
-//        ITEMS.register(eventBus);
     }
+
+
+    static {
+        REGISTRATE.item("engineer_goggles_upgrade", EngineerGogglesUpgradeItem::new).register();
+    }
+
+
+
+//    private static void registerUpgrade(RegistryObject<PNCUpgrade> upgrade, ModUpgrades.BuiltinUpgrade upgradeDetails) {
+//        IntStream.range(1, upgradeDetails.getMaxTier() + 1).forEach(
+//                tier -> {
+//                    String baseName = upgradeDetails.getName() + "_upgrade";
+//                    String itemName = upgradeDetails.getMaxTier() > 1 ? baseName + "_" + tier : baseName;
+//                    register(itemName, () -> new UpgradeItem(upgrade, tier));
+//                }
+//        );
+//    }
 }
---
+++ src/main/java/com/lgmrszd/compressedcreativity/index/CCUpgradesDBSetup.java
@@ -0,0 +1,14 @@
+package com.lgmrszd.compressedcreativity.index;
+
+import me.desht.pneumaticcraft.common.util.upgrade.ApplicableUpgradesDB;
+import me.desht.pneumaticcraft.common.util.upgrade.UpgradesDBSetup;
+
+public class CCUpgradesDBSetup {
+    public static void init() {
+
+        ApplicableUpgradesDB db = ApplicableUpgradesDB.getInstance();
+
+//        UpgradesDBSetup.Builder builder = new UpgradesDBSetup.Builder();
+
+    }
+}
