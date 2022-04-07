edf944db2c0166adb5c449d794078c8a4303ee5d
Lgmrszd <m.v.bobrov@yandex.ru>
2022-04-07 03:45:06 +0300

    Simplified goggles overlay predicate

--- src/main/java/com/lgmrszd/compressedcreativity/GoggledChecker.java
+++ src/main/java/com/lgmrszd/compressedcreativity/GoggledChecker.java
@@ -1,32 +1,16 @@
 package com.lgmrszd.compressedcreativity;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.Tag;
+import me.desht.pneumaticcraft.common.util.UpgradableItemUtils;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-
-import java.util.function.Predicate;
-
-import static me.desht.pneumaticcraft.api.lib.NBTKeys.NBT_UPGRADE_INVENTORY;
+import static me.desht.pneumaticcraft.common.core.ModUpgrades.BLOCK_TRACKER;
 
 public class GoggledChecker {
 
 //    @OnlyIn(Dist.CLIENT)
     public static boolean hasBlockTrackerUpgrade(Player player) {
-
-        // TODO: rewrite using new PNC:R API
         ItemStack helm = player.getItemBySlot(EquipmentSlot.HEAD);
-        if (helm.isEmpty() || !helm.hasTag() || !helm.getTag().contains(NBT_UPGRADE_INVENTORY)) return false;
-        CompoundTag upgradeInventory = helm.getTag().getCompound(NBT_UPGRADE_INVENTORY);
-        ListTag itemList = upgradeInventory.getList("Items", Tag.TAG_COMPOUND);
-        for (int i = 0; i < itemList.size(); i++) {
-            CompoundTag slotEntry = itemList.getCompound(i);
-            if (slotEntry.getString("id").equals("pneumaticcraft:block_tracker_upgrade")) return true;
-        }
-        return false;
+        return UpgradableItemUtils.getUpgrades(helm, BLOCK_TRACKER.get()) > 0;
     }
 }
