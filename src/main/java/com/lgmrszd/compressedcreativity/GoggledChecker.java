af5e7f7312bf300a36bfa6b04b84181a08390769
Lgmrszd <m.v.bobrov@yandex.ru>
2022-05-13 04:10:18 +0300

    Got separate engineer goggles upgrade working

--- src/main/java/com/lgmrszd/compressedcreativity/GoggledChecker.java
+++ src/main/java/com/lgmrszd/compressedcreativity/GoggledChecker.java
@@ -1,16 +1,20 @@
 package com.lgmrszd.compressedcreativity;
 
+import me.desht.pneumaticcraft.api.PneumaticRegistry;
+import me.desht.pneumaticcraft.api.item.IUpgradeRegistry;
 import me.desht.pneumaticcraft.common.util.UpgradableItemUtils;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import static me.desht.pneumaticcraft.common.core.ModUpgrades.BLOCK_TRACKER;
+import static com.lgmrszd.compressedcreativity.index.CCUpgrades.ENGINEER_GOGGLES;
+
 
 public class GoggledChecker {
+//    private static final IUpgradeRegistry upgradeRegistry = PneumaticRegistry.getInstance().getItemRegistry().getUpgradeRegistry();
 
 //    @OnlyIn(Dist.CLIENT)
     public static boolean hasBlockTrackerUpgrade(Player player) {
         ItemStack helm = player.getItemBySlot(EquipmentSlot.HEAD);
-        return UpgradableItemUtils.getUpgradeCount(helm, BLOCK_TRACKER.get()) > 0;
+        return UpgradableItemUtils.getUpgradeCount(helm, ENGINEER_GOGGLES.get()) > 0;
     }
 }
--- src/main/java/com/lgmrszd/compressedcreativity/index/CCUpgrades.java
+++ src/main/java/com/lgmrszd/compressedcreativity/index/CCUpgrades.java
@@ -9,7 +9,7 @@ import static com.lgmrszd.compressedcreativity.CompressedCreativity.MOD_ID;
 import static me.desht.pneumaticcraft.api.PneumaticRegistry.RL;
 
 public class CCUpgrades {
-    public static final DeferredRegister<PNCUpgrade> UPGRADES_DEFERRED = DeferredRegister.create(new ResourceLocation(MOD_ID,"upgrades"), MOD_ID);
+    public static final DeferredRegister<PNCUpgrade> UPGRADES_DEFERRED = DeferredRegister.create(RL("upgrades"), MOD_ID);
 
     public static final RegistryObject<PNCUpgrade> ENGINEER_GOGGLES = UPGRADES_DEFERRED.register(
             "engineer_goggles",
