f1e2135b51fef1a15cf10dd71326dd794c4df4af
Lgmrszd <m.v.bobrov@yandex.ru>
2022-05-14 01:53:59 +0300

    Now it actually works!

--- src/main/java/com/lgmrszd/compressedcreativity/GoggledChecker.java
+++ src/main/java/com/lgmrszd/compressedcreativity/GoggledChecker.java
@@ -1,7 +1,10 @@
 package com.lgmrszd.compressedcreativity;
 
+import com.lgmrszd.compressedcreativity.index.CCCommonUpgradeHandlers;
 import me.desht.pneumaticcraft.api.PneumaticRegistry;
 import me.desht.pneumaticcraft.api.item.IUpgradeRegistry;
+import me.desht.pneumaticcraft.api.pneumatic_armor.ICommonArmorHandler;
+import me.desht.pneumaticcraft.api.pneumatic_armor.ICommonArmorRegistry;
 import me.desht.pneumaticcraft.common.util.UpgradableItemUtils;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
@@ -14,7 +17,8 @@ public class GoggledChecker {
 
 //    @OnlyIn(Dist.CLIENT)
     public static boolean hasBlockTrackerUpgrade(Player player) {
-        ItemStack helm = player.getItemBySlot(EquipmentSlot.HEAD);
-        return UpgradableItemUtils.getUpgradeCount(helm, ENGINEER_GOGGLES.get()) > 0;
+        ICommonArmorRegistry reg = PneumaticRegistry.getInstance().getCommonArmorRegistry();
+        ICommonArmorHandler handler = reg.getCommonArmorHandler(player);
+        return handler.upgradeUsable(CCCommonUpgradeHandlers.engineersGogglesHandler, true);
     }
 }
--- src/main/java/com/lgmrszd/compressedcreativity/index/CCCommonUpgradeHandlers.java
+++ src/main/java/com/lgmrszd/compressedcreativity/index/CCCommonUpgradeHandlers.java
@@ -2,13 +2,16 @@ package com.lgmrszd.compressedcreativity.index;
 
 import com.lgmrszd.compressedcreativity.upgrades.EngineersGogglesHandler;
 // TODO use API
+import me.desht.pneumaticcraft.api.PneumaticRegistry;
+import me.desht.pneumaticcraft.api.pneumatic_armor.ICommonArmorRegistry;
 import me.desht.pneumaticcraft.common.pneumatic_armor.ArmorUpgradeRegistry;
 
 public class CCCommonUpgradeHandlers {
     public static EngineersGogglesHandler engineersGogglesHandler;
 
     public static void init() {
-        ArmorUpgradeRegistry r = ArmorUpgradeRegistry.getInstance();
-        engineersGogglesHandler = r.registerUpgradeHandler(new EngineersGogglesHandler());
+//        ArmorUpgradeRegistry r = ArmorUpgradeRegistry.getInstance();
+        ICommonArmorRegistry reg = PneumaticRegistry.getInstance().getCommonArmorRegistry();
+        engineersGogglesHandler = reg.registerUpgradeHandler(new EngineersGogglesHandler());
     }
 }
