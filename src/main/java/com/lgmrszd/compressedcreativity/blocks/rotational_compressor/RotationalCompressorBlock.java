2c8f91949aa3dff1b10dba377b24146fd67557ae
Lgmrszd <m.v.bobrov@yandex.ru>
2022-05-18 15:45:40 +0300

    Pneumatic block destruction effect using API

--- src/main/java/com/lgmrszd/compressedcreativity/blocks/rotational_compressor/RotationalCompressorBlock.java
+++ src/main/java/com/lgmrszd/compressedcreativity/blocks/rotational_compressor/RotationalCompressorBlock.java
@@ -5,20 +5,13 @@ import com.lgmrszd.compressedcreativity.index.CCShapes;
 import com.simibubi.create.content.contraptions.base.HorizontalKineticBlock;
 import com.simibubi.create.content.contraptions.base.IRotate;
 import com.simibubi.create.foundation.block.ITE;
-import me.desht.pneumaticcraft.api.PNCCapabilities;
-import me.desht.pneumaticcraft.common.core.ModSounds;
-import me.desht.pneumaticcraft.common.network.NetworkHandler;
-import me.desht.pneumaticcraft.common.network.PacketSpawnParticle;
-import me.desht.pneumaticcraft.common.particle.AirParticleData;
-import net.minecraft.world.level.block.Block;
+import me.desht.pneumaticcraft.api.PneumaticRegistry;
+import me.desht.pneumaticcraft.api.misc.IMiscHelpers;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.core.Direction;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -84,15 +77,9 @@ public class RotationalCompressorBlock extends HorizontalKineticBlock implements
     public void onRemove(BlockState state, Level world, BlockPos pos, BlockState newState, boolean isMoving) {
         if (state.getBlock() != newState.getBlock()) {
             BlockEntity te = world.getBlockEntity(pos);
-            if (te instanceof RotationalCompressorTileEntity) {
-                te.getCapability(PNCCapabilities.AIR_HANDLER_MACHINE_CAPABILITY).ifPresent(handler -> {
-                    if (handler.getAir() > 0) {
-                        NetworkHandler.sendToAllTracking(new PacketSpawnParticle(AirParticleData.DENSE, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 0.0D, 0.0D, 0.0D, (int)(5.0F * handler.getPressure()), 1.0D, 1.0D, 1.0D), world, pos);
-                        world.playSound((Player)null, pos, (SoundEvent) ModSounds.SHORT_HISS.get(), SoundSource.BLOCKS, 0.3F, 0.8F);
-//                        world.explode(null, pos.getX() + 0.5F, pos.getY() + 0.5, pos.getZ() + 0.5F, 1.0F, false, Explosion.Mode.BREAK);
-//                        world.explode(null, pos.getX() + 0.5F, pos.getY() + 0.5, pos.getZ() + 0.5F, 1.5F, Explosion.Mode.NONE);
-                    }
-                });
+            if (te instanceof RotationalCompressorTileEntity rcte) {
+                IMiscHelpers miscHelpers = PneumaticRegistry.getInstance().getMiscHelpers();
+                miscHelpers.playMachineBreakEffect(rcte);
             }
         }
         super.onRemove(state, world, pos, newState, isMoving);
