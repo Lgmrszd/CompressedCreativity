a0dd8fba4d6c263fd3293bc90acf66413ebe8a01
Lgmrszd <m.v.bobrov@yandex.ru>
2022-05-19 15:37:04 +0300

    Air Blower updated model

--- src/main/java/com/lgmrszd/compressedcreativity/index/CCShapes.java
+++ src/main/java/com/lgmrszd/compressedcreativity/index/CCShapes.java
@@ -26,11 +26,11 @@ public class CCShapes {
         VoxelShape shape = Shapes.empty();
         shape = Shapes.join(shape, Shapes.box(0, 0, 0.875, 1, 1, 1), BooleanOp.OR);
         shape = Shapes.join(shape, Shapes.box(0.125, 0.125, 0.125, 0.875, 0.875, 0.875), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.3125, 0, 0.6875, 0.6875, 0.125), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.875, 0.3125, 0.3125, 1, 0.6875, 0.6875), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0, 0.3125, 0.3125, 0.125, 0.6875, 0.6875), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.875, 0.3125, 0.6875, 1, 0.6875), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0, 0.3125, 0.6875, 0.125, 0.6875), BooleanOp.OR);
+//        shape = Shapes.join(shape, Shapes.box(0.3125, 0.3125, 0, 0.6875, 0.6875, 0.125), BooleanOp.OR);
+//        shape = Shapes.join(shape, Shapes.box(0.875, 0.3125, 0.3125, 1, 0.6875, 0.6875), BooleanOp.OR);
+//        shape = Shapes.join(shape, Shapes.box(0, 0.3125, 0.3125, 0.125, 0.6875, 0.6875), BooleanOp.OR);
+//        shape = Shapes.join(shape, Shapes.box(0.3125, 0.875, 0.3125, 0.6875, 1, 0.6875), BooleanOp.OR);
+//        shape = Shapes.join(shape, Shapes.box(0.3125, 0, 0.3125, 0.6875, 0.125, 0.6875), BooleanOp.OR);
 
 
         return shape;
