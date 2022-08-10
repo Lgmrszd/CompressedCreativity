41dd587eca7733c25d1820e8729f8bd1ed0fc4e2
Lgmrszd <lgmrszd@gmail.com>
2022-08-10 23:00:21 +0300

    Furnace Engine Heat Handler + config

--- src/main/java/com/lgmrszd/compressedcreativity/heatbehaviour/CCHeatBehaviourFurnace.java
+++ src/main/java/com/lgmrszd/compressedcreativity/heatbehaviour/CCHeatBehaviourFurnace.java
@@ -1,6 +1,6 @@
 package com.lgmrszd.compressedcreativity.heatbehaviour;
 
-import com.lgmrszd.compressedcreativity.CompressedCreativity;
+import com.lgmrszd.compressedcreativity.config.CommonConfig;
 import com.simibubi.create.content.contraptions.components.flywheel.engine.FurnaceEngineTileEntity;
 import me.desht.pneumaticcraft.api.heat.HeatBehaviour;
 import net.minecraft.core.BlockPos;
@@ -12,34 +12,16 @@ import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.event.world.ExplosionEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import static com.lgmrszd.compressedcreativity.index.CCMisc.OVERCLOCKED_ENGINE;
 import static me.desht.pneumaticcraft.api.PneumaticRegistry.RL;
 
+
+/* Copied from PNC:R code and modified to override original behaviour */
+
 public class CCHeatBehaviourFurnace extends HeatBehaviour {
 
-    public enum FurnaceEngineHeatSettings {
-        NONE(0),
-        STALL(0),
-        MISCHIEF(1),
-        EVIL(2);
-
-        private final float strength;
-        public float getExplosionStrength() {
-            return strength;
-        }
-        FurnaceEngineHeatSettings(float explosionStrength) {
-            strength = explosionStrength;
-        }
-    }
-
     static final ResourceLocation ID = RL("furnace");
-    
-    private boolean wasFurnaceEngineConnected = false;
     private int furnaceEngineConnectedTicks = 0;
 
     private boolean isFurnaceEngineConnected() {
@@ -77,8 +59,6 @@ public class CCHeatBehaviourFurnace extends HeatBehaviour {
                 furnaceEngineConnectedTicks = furnaceEngineConnectedTicks + 1;
                 BlockPos furnacePos = getCachedTileEntity().getBlockPos();
                 if (getWorld() instanceof ServerLevel serverLevel) {
-//                        serverLevel.getEntities()
-//                    serverLevel.sendParticles(ParticleTypes.POOF, furnacePos.getX() + 0.5F, furnacePos.getY()  + 1F, furnacePos.getZ() + 0.5F, 2, 0.0D, 0.5D, 0.0D, 0.1D);
                     if (furnaceEngineConnectedTicks % 2 == 0) {
                         serverLevel.sendParticles(ParticleTypes.POOF, furnacePos.getX() + 0.5F, furnacePos.getY()  + 1F, furnacePos.getZ() + 0.5F, 2, 0.0D, 0.5D, 0.0D, 0.1D);
                     }
@@ -89,20 +69,13 @@ public class CCHeatBehaviourFurnace extends HeatBehaviour {
                         serverLevel.sendParticles(ParticleTypes.FLAME, furnacePos.getX() + 0.5F, furnacePos.getY()  + 1F, furnacePos.getZ() + 0.5F, 20, 0.2D, 0.5D, 0.2D, 0.3D);
                     }
                 }
-//                    getWorld().addParticle(ParticleTypes.POOF, furnacePos.getX() + 0.5F, furnacePos.getY()  + 1F, furnacePos.getZ() + 0.5F, 0.0D, 0.0D, 0.0D);
-                if (furnaceEngineConnectedTicks > 800) {
-                    Logger logger = LogManager.getLogger(CompressedCreativity.MOD_ID);
-                    logger.debug("Furnace engine detected!");
+                if (furnaceEngineConnectedTicks > 140 && CommonConfig.FURNACE_ENGINE_EXPLODE.get()) {
                     getWorld().destroyBlock(furnacePos, true);
-//                    getWorld().explode(null, furnacePos.getX() + 0.5F, furnacePos.getY() + 0.5, furnacePos.getZ() + 0.5F, 2F, false, Explosion.BlockInteraction.BREAK);
-//                    getWorld().explode(null, furnacePos.getX() + 0.5F, furnacePos.getY() + 0.5, furnacePos.getZ() + 0.5F, 2F, false, Explosion.BlockInteraction.NONE);
                     getWorld().explode(null, OVERCLOCKED_ENGINE, null, furnacePos.getX() + 0.5F, furnacePos.getY() + 0.5, furnacePos.getZ() + 0.5F, 1F, false, Explosion.BlockInteraction.NONE);
-                    wasFurnaceEngineConnected = true;
                 }
                 return;
             }
-            furnaceEngineConnectedTicks = 0;
-            wasFurnaceEngineConnected = false;
+            furnaceEngineConnectedTicks = Math.max(0, furnaceEngineConnectedTicks - 2);
             if (furnace.litTime < 190 && !furnace.getItem(0).isEmpty()) {
                 if (furnace.litTime == 0) {
                     getWorld().setBlockAndUpdate(getPos(), getBlockState().setValue(AbstractFurnaceBlock.LIT, true));
